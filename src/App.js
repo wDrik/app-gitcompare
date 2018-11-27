0f5b68ff9ca9d4fcbed8dbc82b555deef207cf23
Iorgen <w.drik@outlook.com>
2018-11-27 20:31:17 -0200

    config ESLINT

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App"></div>
+      <div className="App">
+        <h1>Hello world!</h1>
+      </div>
     );
   }
 }
