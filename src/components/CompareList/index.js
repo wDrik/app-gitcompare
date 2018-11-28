41e5a1b482c6bda1a69fe86c4242e8c11428bb25
Iorgen <w.drik@outlook.com>
2018-11-28 00:41:59 -0200

    Integration github API

--- src/components/CompareList/index.js
+++ src/components/CompareList/index.js
@@ -2,60 +2,40 @@ import React from 'react';
 
 import { Container, Repository } from './styles';
 
-const CompareList = () => (
+const CompareList = ({ repositories }) => (
   <Container>
-    <Repository>
-      <header>
-        <img src="https://avatars3.githubusercontent.com/u/69631?v=4" alt="facebook"/>
-        <strong>React</strong>
-        <small>Facebook</small>
-      </header>
 
-      <ul>
-        <li>
-          95,018 <small>stars</small>
-        </li>
+    { repositories.map(repository => (
+      <Repository key={ repository.id }>
+        <header>
+          <img src={ repository.owner.avatar_url } alt={ repository.owner.login } />
+          <strong>{ repository.name }</strong>
+          <small>{ repository.owner.login }</small>
+        </header>
 
-        <li>
-          95,018 <small>forks</small>
-        </li>
+        <ul>
+          <li>
+            { repository.stargazers_count } <small>stars</small>
+          </li>
 
-        <li>
-          95,018 <small>issues</small>
-        </li>
+          <li>
+            { repository.forks_count } <small>forks</small>
+          </li>
 
-        <li>
-          3 days ago <small>last commit</small>
-        </li>
-      </ul>
-    </Repository>
+          <li>
+            { repository.open_issues_count } <small>issues</small>
+          </li>
 
-    <Repository>
-      <header>
-        <img src="https://avatars3.githubusercontent.com/u/69631?v=4" alt="facebook"/>
-        <strong>React</strong>
-        <small>Facebook</small>
-      </header>
+          <li>
+            { repository.pushed_at } <small>last commit</small>
+          </li>
+        </ul>
+      </Repository>
+    )) }
 
-      <ul>
-        <li>
-          95,018 <small>stars</small>
-        </li>
-
-        <li>
-          95,018 <small>forks</small>
-        </li>
-
-        <li>
-          95,018 <small>issues</small>
-        </li>
-
-        <li>
-          3 days ago <small>last commit</small>
-        </li>
-      </ul>
-    </Repository>
   </Container>
 );
 
 export default CompareList;
+
+// https://api.github.com/repos/
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { Component } from 'react';
+import api from '../../services/api';
 
 import logo from '../../assets/img/logo.svg';
 
@@ -6,17 +7,47 @@ import { Form, Container } from "./styles";
 
 import CompareList from '../../components/CompareList';
 
-const Main = () => (
-  <Container>
-    <img src={ logo } className="logo" alt="Github" />
 
-    <Form>
-      <input type="text" placeholder="usuário/repositório" />
-      <button type="submit">OK</button>
-    </Form>
+class Main extends Component {
+  state = {
+    repositoryInput: '',
+    repositories: []
+  }
 
-    <CompareList />
-  </Container>
-);
+  handleAddRepository = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await api.get(`/repos/${this.state.repositoryInput}`);
+
+      this.setState({
+        repositoryInput: '',
+        repositories: [...this.state.repositories, response.data]
+      })
+    } catch(err) {
+      console.log(err);
+    }
+  }
+
+  render() {
+    return(
+      <Container>
+        <img src={ logo } className="logo" alt="Github" />
+
+        <Form onSubmit={ this.handleAddRepository }>
+          <input
+            type="text"
+            placeholder="usuário/repositório"
+            value={ this.state.repositoryInput }
+            onChange={ e => this.setState({ repositoryInput: e.target.value }) }
+          />
+          <button type="submit">OK</button>
+        </Form>
+
+        <CompareList repositories={ this.state.repositories } />
+      </Container>
+    );
+  }
+}
 
 export default Main;
